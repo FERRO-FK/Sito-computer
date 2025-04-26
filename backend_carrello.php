066e2fc21ab556716dd2c41f0b5214c0882f8610
FERRO <mattyferro694@gmail.com>
2025-04-26 17:50:10 +0200

    creazione del carrello e prodotti

---
+++ backend_carrello.php
@@ -0,0 +1,107 @@
+<?php
+session_start();
+header('Content-Type: application/json');
+
+// Configurazione CORS per sviluppo
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+// Database dei prodotti
+$prodotti = [
+    1 => ["id" => 1, "nome" => "Notebook da ufficio", "prezzo" => 499.00],
+    2 => ["id" => 2, "nome" => "PC Gaming", "prezzo" => 899.00],
+    3 => ["id" => 3, "nome" => "Workstation", "prezzo" => 1299.00],
+    4 => ["id" => 4, "nome" => "Ultrabook", "prezzo" => 799.00],
+    5 => ["id" => 5, "nome" => "All-in-One", "prezzo" => 1099.00],
+];
+
+// Inizializza il carrello se non esiste
+if (!isset($_SESSION['carrello'])) {
+    $_SESSION['carrello'] = [];
+}
+
+$action = $_GET['action'] ?? '';
+
+switch ($action) {
+    case 'getCart':
+        getCart();
+        break;
+        
+    case 'updateQuantity':
+        updateQuantity();
+        break;
+        
+    case 'removeItem':
+        removeItem();
+        break;
+        
+    default:
+        echo json_encode(['success' => false, 'message' => 'Azione non valida']);
+}
+
+function getCart() {
+    global $prodotti;
+    
+    $cartItems = [];
+    $totale = 0.0;
+    
+    foreach ($_SESSION['carrello'] as $id => $quantita) {
+        if (isset($prodotti[$id])) {
+            $prodotto = $prodotti[$id];
+            $subtotale = $prodotto['prezzo'] * $quantita;
+            
+            $cartItems[] = [
+                'id' => $prodotto['id'],
+                'nome' => $prodotto['nome'],
+                'prezzo' => $prodotto['prezzo'],
+                'quantita' => $quantita,
+                'subtotale' => $subtotale
+            ];
+            
+            $totale += $subtotale;
+        }
+    }
+    
+    echo json_encode([
+        'success' => true,
+        'cartItems' => $cartItems,
+        'totale' => $totale
+    ]);
+}
+
+function updateQuantity() {
+    global $prodotti;
+    
+    $id = (int)($_GET['id'] ?? 0);
+    $change = (int)($_GET['change'] ?? 0);
+    
+    if (!isset($prodotti[$id])) {
+        echo json_encode(['success' => false, 'message' => 'Prodotto non trovato']);
+        return;
+    }
+    
+    if (!isset($_SESSION['carrello'][$id])) {
+        $_SESSION['carrello'][$id] = 1;
+    } else {
+        $_SESSION['carrello'][$id] += $change;
+        
+        // Impedisce quantità negative
+        if ($_SESSION['carrello'][$id] < 1) {
+            $_SESSION['carrello'][$id] = 1;
+        }
+    }
+    
+    echo json_encode(['success' => true]);
+}
+
+function removeItem() {
+    $id = (int)($_GET['id'] ?? 0);
+    
+    if (isset($_SESSION['carrello'][$id])) {
+        unset($_SESSION['carrello'][$id]);
+    }
+    
+    echo json_encode(['success' => true]);
+}
+?>
